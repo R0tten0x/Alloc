e3c7757bc626e128c5b48c709692bb815576d655
Robert | R0tten0x <california.beatdown@icloud.com>
2026-08-18 06:59:58 -0700

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use crate::memory::fetch_pressure;
+
 mod memory;
 mod menubar;
 
 fn main() {
     println!("Hello, world!");
+    fetch_pressure();
 }
--- src/memory.rs
+++ src/memory.rs
@@ -1,26 +1,2 @@
 use std::ffi::CString;
 use std::mem;
-
-pub enum MemmoryPressure {
-    Good,
-    High,
-    Critical,
-}
-
-pub fn poll_memory() -> Option<i32> {
-    let name = CString::new("vm.memory_pressure").ok()?;
-    let mut val: i32 = 0;
-    let mut size = mem::size_of::<i32>() as libc::size_t;
-
-    unsafe {
-        let ret = libc::sysctlbyname(
-            name.as_ptr(),
-            (&mut val as *mut i32) as *mut libc::c_void,
-            &mut size,
-            std::ptr::null_mut(),
-            0,
-        );
-
-        if ret == 0 { Some(val) } else { None }
-    }
-}
